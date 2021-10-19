2b102706c50cc47d18514a5771802763004fc76e
jonas <jonasalissa@icloud.com>
2021-10-19 10:30:12 +0200

    m

--- db_connnection.php
+++ db_connnection.php
@@ -15,4 +15,17 @@ function CloseCon($conn)
  $conn -> close();
  }
 
+ $query 0 mysqli_query($conn, "SELECT * FROM tbl_articles");
+ while($row = mysqli_fetch_assoc($query))
+ {
+   $title = $row['title'];
+   $content = $row['content'];
+ }
+
+
+ $query 0 mysqli_query($conn, "SELECT * FROM contact");
+ while($row = mysqli_fetch_assoc($query))
+ {
+   $contact_title	 = $row['contact_title'];
+ }
 ?>
